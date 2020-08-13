f89d7debe557dfe77ca86ddd41079009cafef971
Textus <textusgames@gmail.com>
2020-08-13 18:10:59 +0300

    Added not supported types example. Classes that has no empty parameters are not supported and not shown(activator can not create instances of them).

--- Assets/Textus/SerializeReferenceUIExample/ExampleOfUsageFromDifferentAssembly/NotSupportedTypes/ExampleOfNotSupportedTypes.cs
+++ Assets/Textus/SerializeReferenceUIExample/ExampleOfUsageFromDifferentAssembly/NotSupportedTypes/ExampleOfNotSupportedTypes.cs
@@ -9,19 +9,19 @@ public class ExampleOfNotSupportedTypes
 [Serializable]
 public class AnimalMonoBehaviour : MonoBehaviour, IAnimal
 {
-    public void Feed() => throw new NotImplementedException();
+    public void Feed() => Debug.Log("thanks");
 }
 
 [Serializable]
 public class AnimalScriptableObject : ScriptableObject, IAnimal
 {
-    public void Feed() => throw new NotImplementedException();
+    public void Feed() => Debug.Log("thanks");
 }
 
 [Serializable]
 public class AnimalPrivateConstructor : IAnimal
 {
     private AnimalPrivateConstructor(){}
-    public void Feed() => throw new NotImplementedException();
+    public void Feed() => Debug.Log("thanks");
 }
 
